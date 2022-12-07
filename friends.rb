b90cd906008e4f5b4ee7064312780eb8c444dd49
Andrew Bingham <andrewgbingham@gmail.com>
2022-12-06 17:45:53 -0700

    add arrays

--- friends.rb
+++ friends.rb
@@ -1,2 +1,8 @@
 arrays = ["Jacob", "James", "Jimmy", "Jerome", "Jerimah"]
 puts arrays[0]
+
+places_to_visit = ["upstate_new_york", "SLC", "Texas", "Colorado", "Cancun"]
+places_to_visit.each do |place|
+   puts place.reverse 
+end
+ 
